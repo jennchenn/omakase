3e805258fb2bd03c5fb51e334c728c2466659b8f
Jennifer Chen <jenn.chenn93@gmail.com>
2021-05-24 02:02:46 -0400

    fix(calendar): adjust time slot finding algorithm

--- backend/src/routes/calendar/calendar.controller.js
+++ backend/src/routes/calendar/calendar.controller.js
@@ -18,46 +18,103 @@ class CalendarController {
     }
 
     findFirstAvailableSlot(availability, meetingLengthMinutes) {
-        const sortedAvailability = this.sortAvailability(availability);
-        const mergedTimes = this.mergeOverlappingSlots(sortedAvailability);
-        console.log(mergedTimes);
+        try {
+            const sortedAvailability = this.sortAvailability(availability);
+            const mergedTimes = this.mergeOverlappingSlots(sortedAvailability);
+            console.log(mergedTimes);
 
+            let proposedMeetingStart = this.addMinutesToDate(moment(), 30);
+            proposedMeetingStart = this.rountToNearestHalfHour(proposedMeetingStart);
+            proposedMeetingStart = moment(proposedMeetingStart);
+            let counter = 0;
+            if (mergedTimes.length > 0 && proposedMeetingStart.isBetween(mergedTimes[0][0], mergedTimes[0][1])) {
+                proposedMeetingStart = mergedTimes[0][1];
+                counter += 1;
+            }
+
+            while (moment(proposedMeetingStart) < moment().add(14, 'days')) {
+                let maxTime = mergedTimes.length > counter ? new Date(mergedTimes[counter][0]) : moment().add(14, 'days');
+                console.log('START', proposedMeetingStart, maxTime);
+                let res = this.findAvailability(moment(proposedMeetingStart), maxTime, meetingLengthMinutes);
+                if (res !== null) return res;
+                proposedMeetingStart = mergedTimes[counter][1];
+                counter += 1;
+            }
+
+            // for (let i = 0; i < mergedTimes.length; i += 1) {
+            //     let proposedMeetingStart = new Date(mergedTimes[i][1]);
+            //     let maxTime = i + 1 < mergedTimes.length ? new Date(mergedTimes[i][0]) : moment().add(14, 'days');
+            //     console.log(proposedMeetingStart, maxTime);
+            //     let res = this.findAvailability(proposedMeetingStart, maxTime, meetingLengthMinutes);
+            //     console.log(res);
+
+            //     if (res !== null) return res;
+            //     // console.log(proposedMeetingStart);
+            //     // if (this.isTimeInBetween(proposedMeetingStart, minTime, endTime)) {
+            //     //     let proposedMeetingEnd = this.addMinutesToDate(proposedMeetingStart, meetingLengthMinutes);
+            //     //     if (proposedMeetingEnd <= new Date(mergedTimes[i + 1][0]) && this.isTimeInBetween(proposedMeetingEnd, minTime, endTime)) {
+            //     //         return { start: proposedMeetingStart, end: proposedMeetingEnd };
+            //     //     }
+            //     // } else {
+            //     //     let temp = moment.utc(proposedMeetingStart);
+            //     //     proposedMeetingStart = moment.utc(proposedMeetingStart);
+            //     //     temp.hours(12);
+            //     //     temp.minutes(30);
+            //     //     let duration = moment.duration(temp.diff(proposedMeetingStart));
+            //     //     let hours = duration.asHours();
+            //     //     proposedMeetingStart.add(hours, 'hours');
+            //     //     if (hours < 0) {
+            //     //         proposedMeetingStart.add(24, 'hours');
+            //     //     }
+            //     // }
+            // }
+            // TODO: handle no availabilities within two weeks
+            return null;
+        } catch (err) {
+            console.log(err);
+            throw new Error(err);
+        }
+    }
+
+    findAvailability(proposedMeetingStart, maxTime, meetingLengthMinutes) {
         const minTime = new Date();
         minTime.setUTCHours(12, 30, 0);
         const endTime = new Date();
         endTime.setUTCHours(3, 0, 0);
-
-        let proposedMeetingStart = this.addMinutesToDate(moment(), 30);
-        proposedMeetingStart = this.rountToNearestHalfHour(proposedMeetingStart);
-
-        while (proposedMeetingStart < new Date(mergedTimes[0][0])) {
+        console.log(proposedMeetingStart, maxTime);
+        while (proposedMeetingStart < maxTime) {
+            console.log(proposedMeetingStart);
             if (this.isTimeInBetween(proposedMeetingStart, minTime, endTime)) {
                 let proposedMeetingEnd = this.addMinutesToDate(proposedMeetingStart, meetingLengthMinutes);
-
-                if (proposedMeetingEnd <= new Date(mergedTimes[0][0]) && this.isTimeInBetween(proposedMeetingEnd, minTime, endTime)) {
+                if (proposedMeetingEnd <= maxTime && this.isTimeInBetween(proposedMeetingEnd, minTime, endTime)) {
+                    console.log({ start: proposedMeetingStart, end: proposedMeetingEnd });
                     return { start: proposedMeetingStart, end: proposedMeetingEnd };
                 } else {
                     break;
                 }
             } else {
-                proposedMeetingStart.setUTCHours(12, 30, 0);
-            }
-        }
-
-        for (let i = 0; i < mergedTimes.length - 1; i += 1) {
-            let proposedMeetingStart = new Date(mergedTimes[i][1]);
-            if (this.isTimeInBetween(proposedMeetingStart, minTime, endTime)) {
-                let proposedMeetingEnd = this.addMinutesToDate(proposedMeetingStart, meetingLengthMinutes);
-                if (proposedMeetingEnd <= new Date(mergedTimes[i + 1][0]) && this.isTimeInBetween(proposedMeetingEnd, minTime, endTime)) {
-                    return { start: proposedMeetingStart, end: proposedMeetingEnd };
+                let temp = moment.utc(proposedMeetingStart);
+                proposedMeetingStart = moment.utc(proposedMeetingStart);
+                console.log('OG', proposedMeetingStart);
+                temp.hours(12);
+                temp.minutes(30);
+                let duration = moment.duration(temp.diff(proposedMeetingStart)).asHours();
+                proposedMeetingStart.add(duration, 'hours');
+                console.log('SHIFTED', proposedMeetingStart, duration);
+                if (proposedMeetingStart < temp) {
+                    proposedMeetingStart.add(24, 'hours');
+                    console.log('SHIFT 24', proposedMeetingStart);
                 }
+                console.log(proposedMeetingStart);
             }
         }
-        // TODO: handle no availabilities within two weeks
-        return;
+        return null;
     }
 
     sortAvailability(availability) {
+        if (availability.length == 0) {
+            return [];
+        }
         return availability.sort((a, b) => {
             let dateA = new Date(a.start);
             let dateB = new Date(b.start);
@@ -66,6 +123,9 @@ class CalendarController {
     }
 
     mergeOverlappingSlots(availability) {
+        if (availability.length == 0) {
+            return [];
+        }
         const mergedTimes = [];
         let previousEvent = [availability[0].start, availability[0].end];
         for (let i = 1; i < availability.length; i += 1) {
@@ -91,9 +151,12 @@ class CalendarController {
         end.hour(6);
         end.minute(30);
         if (start.isAfter(end)) {
-            start.subtract(1, 'days');
+            end.add(1, 'days');
         }
-        return time.isAfter(start) && time.isBefore(end);
+        console.log('IS BETWEEN', start, end, time);
+        console.log(time.isSameOrAfter(start));
+        console.log(time.isSameOrBefore(end));
+        return time.isSameOrAfter(start) && time.isSameOrBefore(end);
     }
 
     addMinutesToDate(date, minutes) {
